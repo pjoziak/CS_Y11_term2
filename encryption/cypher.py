a421cc33cbba31e338e08ae5ad6cb7be2db0b375
Paweł Józiak <pjoziak@gmail.com>
2022-04-04 11:22:32 +0200

    Add Cypher abstract class

--- encryption/cypher.py
+++ encryption/cypher.py
@@ -6,6 +6,7 @@ from encryption.key import Key
 class Cypher:
     def __init__(self, key: Key):
         self.key = key
+        print(f'Created Cypher object with key: {key}')
 
     @abstractmethod
     def encrypt(self, message: str) -> str:
