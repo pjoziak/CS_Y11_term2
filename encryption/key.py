ad198f01ff9d0f0545c345733ab33e4a7a147959
Paweł Józiak <pjoziak@gmail.com>
2022-04-04 11:22:20 +0200

    Add type annotations

--- encryption/key.py
+++ encryption/key.py
@@ -2,15 +2,18 @@ DEADLINE = '2022-04-18'
 
 
 class Key:
-    def __init__(self, key):  # dunder method == double underscore
+    def __init__(self, key: str):  # dunder method == double underscore
         self.key = ''.join(c for c in key if c.isalpha()).upper()
         self.counter = 0
         self.length = len(self.key)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.length
 
-    def __iter__(self):  # to allow infinite wrap-around
+    def __str__(self) -> str:
+        return f'Key(key={self.key})'
+
+    def __iter__(self) -> str:  # to allow infinite wrap-around
         while True:
             if self.counter >= self.length:
                 self.counter = 0
